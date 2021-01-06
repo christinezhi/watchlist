d0a44bb8bd0f7b7f9f74dab7c50f3279d20d652d
pika <christinezhi523@gmail.com>
2021-01-06 14:38:35 -0500

    Add database support with Flask-SQLalchemy

--- env/app.py
+++ env/app.py
@@ -1,7 +1,94 @@
-from flask import Flask, render_template  # 首先我们从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：
+import os
+import sys
+
+from flask import Flask, render_template, url_for  # 首先我们从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：
 from flask import escape
+from flask_sqlalchemy import SQLAlchemy
+
+"""WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////' """
+prefix = 'sqlite:///'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')  # sqlite:////数据库文件的绝对地址
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False  # 关闭对模型的修改的追踪
+# 在拓展类实例化前加载配置
+db = SQLAlchemy(app)
+
+# 创建数据库模型
+"""class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # primary key
+    name = db.Column(db.String(20))  # name
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+"""
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+db.create_all()
+
+import click
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database"""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database')
+
+
+import click
+
+
+@app.cli.command()  # 注册为命令
+def forge():
+    """Generate fake data"""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Pikapika'
+    movie = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movie:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done')
+
 
 '''@app.route('/')  # app.route() 装饰器来为这个函数绑定对应的 URL
 def hello():
@@ -11,6 +98,8 @@ def hello():
 
 @app.route('/')
 def index():
+    user = User.query.first()
+    movie = Movie.query.all()
     return render_template('index.html', name=name, movies=movies)
 
 
@@ -30,18 +119,3 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
-
-
-name = 'Pikapika'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
