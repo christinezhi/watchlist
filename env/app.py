0753b9a86268f273e10c6b7bbce376bdfb753d6d
pika <christinezhi523@gmail.com>
2021-01-05 23:19:46 -0500

    Add minimal home page

---
+++ env/app.py
@@ -0,0 +1,27 @@
+from flask import Flask  # 首先我们从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：
+from flask import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')  # app.route() 装饰器来为这个函数绑定对应的 URL
+def hello():
+    return '<h1>Hello, welcome to my watchlist! Pippi loves you!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
